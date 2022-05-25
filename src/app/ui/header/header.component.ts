b300d4f8d1159a9a8dd823c9c2a0bdc024f863b7
Norweb <contacto@norweb.cl>
2022-05-25 18:41:29 -0400

    fix unsubscribe

--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription } from 'rxjs';
 import { AppComponent } from '../../app.component';
 import { AuthService } from '../../core/auth.service';
 import { NotificacionesService } from '../../core/notificaciones.service';
@@ -13,18 +14,22 @@ import { NotificacionesService } from '../../core/notificaciones.service';
 export class HeaderComponent implements OnInit {
   selected = 'Brisas del Norte';
   notificaciones: any;
+  subs:Subscription;  
   constructor(public ac: AppComponent, private modalService: NgbModal, public snackBar: MatSnackBar, public authService: AuthService,
     private router: Router, public notiService: NotificacionesService,
     private activatedRouter: ActivatedRoute) { }
   ngOnInit() {
-
-    this.authService.user.subscribe(user => {
-      this.notificaciones = this.notiService.getMisNotificaciones(user.uid);
+    this.subs = this.authService.user.subscribe(user => {
+      if(user){
+        console.log(user.uid);
+        this.notificaciones = this.notiService.getMisNotificaciones(user.uid);
+      }
     });
   }
   onLogout() {
+    this.subs.unsubscribe();
     this.authService.salir();
-    this.router.navigate(['/inicio'])
+    this.router.navigate(['/inicio']);
   }
   salir() {
     this.snackBar.open('Has cerrado sesión correctamente, vuelve pronto. ', 'CERRAR', {
